fb2eda76b71f97e0f4ff824fcb47dff8668efd71
tjrdbfl <tjrdbfl123@gmail.com>
2024-03-07 12:00:41 +0900

    feat(add findUsersByJobFromMap)

--- src/user/UserRepository.java
+++ src/user/UserRepository.java
@@ -2,9 +2,26 @@ package user;
 
 import user.UserService;
 
+import java.sql.*;
+
 public class UserRepository {
 
 
-    public void addUsers() {
+    public void findUsers() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/dennisdb";
+        String userName = "root";
+        String password = "rootroot";
+
+        Connection connection = DriverManager.getConnection(url, userName, password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from board");
+
+        resultSet.next();
+        String name = resultSet.getString("name");
+        System.out.println(name);
+
+        resultSet.close();
+        statement.close();
+        connection.close();
     }
 }
