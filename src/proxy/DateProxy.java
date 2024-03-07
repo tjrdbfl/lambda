e7a03a39388d7c6dc2870d81a7522aeba4a3c7cb
tjrdbfl <tjrdbfl123@gmail.com>
2024-03-07 17:49:35 +0900

    feat(add Proxy)

---
+++ src/proxy/DateProxy.java
@@ -0,0 +1,35 @@
+package proxy;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class DateProxy {
+    //Date Constructor
+    //현재 날짜
+    public static Supplier<LocalDate> localDateNow= LocalDate::now;
+
+    //문자열로부터 날짜
+    public static Function<String,LocalDate> dateTimeFormatter= a-> {
+        return LocalDate.parse(a,DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    };
+    //문자열 변환
+    public static Function<LocalDate,String> dateToString=LocalDate::toString;
+//    public static Function<String,LocalDate> dateTimeFormatter=Da::toString;
+
+    //날짜 정보 접근
+    public static Supplier<Integer> year=localDateNow.get()::getYear;
+    public static Supplier<Integer> month=localDateNow.get()::getMonthValue;
+    public static Supplier<Integer> dayOfMonth=localDateNow.get()::getDayOfMonth;
+    public static Supplier<DayOfWeek> dayOfWeek=localDateNow.get()::getDayOfWeek;
+
+    //날짜 조작
+    public static BiFunction<LocalDate,LocalDate,Boolean> isBefore= LocalDate::isBefore;
+    public static BiFunction<LocalDate,LocalDate,Boolean> isAfter= LocalDate::isAfter;
+    public static BiFunction<LocalDate,LocalDate,Boolean> isEqual= LocalDate::isEqual;
+
+}
---
+++ src/proxy/MathProxy.java
@@ -0,0 +1,56 @@
+package proxy;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class MathProxy {
+    //Math.abs
+    public static Function<Integer,Integer> absInteger = Math::abs;
+    public static Function<Double,Double> absDouble = Math::abs;
+    public static Function<Float,Float> absFloat = Math::abs;
+    public static Function<Long,Long> absLong = Math::abs;
+
+    //Math.max
+    public static BiFunction<Integer,Integer,Integer> maxInteger = Math::max;
+    public static BiFunction<Double,Double,Double> maxDouble = Math::max;
+    public static BiFunction<Float,Float,Float> maxFloat = Math::max;
+    public static BiFunction<Long,Long,Long> maxLong = Math::max;
+
+    //Math.min
+    public static BiFunction<Integer,Integer,Integer> minInteger = Math::min;
+    public static BiFunction<Double,Double,Double> minDouble = Math::min;
+    public static BiFunction<Float,Float,Float> minFloat = Math::min;
+    public static BiFunction<Long,Long,Long> minLong = Math::min;
+
+    //Math.random - supplier
+    public static Supplier<Double> randomSupplierDouble=Math::random;
+
+    //Math.random - function
+    public static BiFunction<Integer,Integer,Integer> randomInteger = (a,b)->(int)(Math.random()*(b-a)+a);
+    public static BiFunction<Double,Double,Double> randomDouble = (a,b)->(Math.random()*(b-a)+a);
+    public static BiFunction<Float,Float,Float> randomFlaot = (a,b)->(float)(Math.random()*(b-a)+a);
+    public static BiFunction<Long,Long,Long> randomLong = (a,b)->(long)(Math.random()*(b-a)+a);
+
+    //Math.ceil
+    public static Function<Double,Double> ceilDouble =Math::ceil;
+    public static Function<Float,Float> ceilFloat =a-> (float) Math.ceil(a);
+    public static Function<Long,Long> ceilLong =a-> (long) Math.ceil(a);
+
+    //Math.floor
+    public static Function<Double,Double> floorDouble =Math::floor;
+    public static Function<Float,Float> floorFloat =a-> (float) Math.floor(a);
+    public static Function<Long,Long> floorLong =a-> (long) Math.floor(a);
+
+    //Math.round
+    public static Function<Double,Double> roundDouble =a->(double) Math.round(a);
+    public static Function<Float,Float> roundFloat =a->(float) Math.round(a);
+    public static Function<Long,Long> roundLong =a->(long) Math.round(a);
+
+    //Integer.parseInt
+    public static Function<String,Integer> parseInt = Integer::parseInt;
+    public static Function<String,Double> parseDouble = Double::parseDouble;
+    public static Function<String,Float> parseFloat =Float::parseFloat;
+    public static Function<String,Long> parseLong = Long::parseLong;
+
+}
