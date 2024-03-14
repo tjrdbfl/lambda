89262b0cabd8ebb4ebda45a87f09826cf28da904
tjrdbfl <tjrdbfl123@gmail.com>
2024-03-14 20:11:40 +0900

    style(method type change to messenger)

--- src/com/dennis/api/user/UserServiceImpl.java
+++ src/com/dennis/api/user/UserServiceImpl.java
@@ -4,135 +4,57 @@ import com.dennis.api.common.AbstractService;
 import com.dennis.api.common.UtilServiceImpl;
 import com.dennis.api.enums.Messenger;
 
+import java.sql.SQLException;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class UserServiceImpl extends AbstractService<User> implements UserService {
+public class UserServiceImpl implements UserService {
     private static UserServiceImpl instance = new UserServiceImpl();
-    Map<String, User> users;
+    private UserRepository userRepository;
 
     private UserServiceImpl(){
-        this.users = new HashMap<>();
+        userRepository=UserRepository.getInstance();
     }
     public static UserServiceImpl getInstance(){return instance;}
+
     @Override
-    public Messenger save(User user) {
-        users.put(user.getUsername(), user);
-        return Messenger.SUCCESS;
+    public Messenger addUsers(String a) {
+        return userRepository.create();
     }
 
     @Override
-    public List<User> findAll() {
-        return  new ArrayList<>(users.values());
+    public Messenger login(User user) {
+        return null;
     }
 
     @Override
-    public String login(User user) {
-        return users.getOrDefault(user.getUsername(), User.builder().password("").build())
-                .getPassword()
-                .equals(user.getPassword()) ?
-                "로그인 성공" : "로그인 실패";
+    public Messenger updatePassword(User user) {
+        return null;
     }
 
     @Override
-    public Optional<User> findById(Long id) {
-        return Optional.of(users
-                .values()
-                .stream()
-                .filter(i -> i.getId().equals(id))
-                .collect(Collectors.toList()).get(0));
+    public Messenger findUsersByName(String name) {
+        return null;
     }
 
     @Override
-    public String updatePassword(User user) {
-        users.get(user.getUsername()).setPassword(user.getPassword());
-
-        return "비번 변경 성공";
+    public Messenger findUsersByNameFromMap(String name) {
+        return null;
     }
 
     @Override
-    public Messenger delete(User user) {
-        users.remove(user.getUsername());
-        return Messenger.SUCCESS;
+    public Messenger findUsersByJob(String job) {
+        return null;
     }
 
     @Override
-    public Boolean existsById(Long id) {
-        return users.containsKey(id);
-    }
-
-
-
-    @Override
-    public List<?> findUsersByName(String name) {
-        return users
-                .values()
-                .stream()
-                .filter(i -> i.getName().equals(name))
-                .collect(Collectors.toList());
+    public Messenger findUsersByJobFromMap(String job) {
+        return null;
     }
 
     @Override
-    public Map<String, ?> findUsersByNameFromMap(String name) {
-
-        return users
-                .entrySet()
-                .stream()
-                .filter(i -> i.getKey().equals(name))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
-                ;
-    }
-
-    @Override
-    public List<?> findUsersByJob(String job) {
-        System.out.println("findUsersByJob 파라미터 : "+job);
-        users
-                .values()
-                .stream().forEach(i->System.out.println("직업 :"+i.getJob()));
-        return users
-                .values()
-                .stream()
-                .filter(i -> i.getJob().equals(job))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public Map<String, ?> findUsersByJobFromMap(String job) {
-        return users
-                .entrySet()
-                .stream()
-                .filter(i -> i.getKey().equals(job))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
-                ;
-    }
-
-    @Override
-    public String count() {
-        return users.size()+"";
-    }
-
-    @Override
-    public Optional<User> getOne(String id) {
-        return Optional.of(users.get(id));
-    }
-
-    @Override
-    public Map<String, ?> getUserMap() {
-        return users;
-    }
-
-    @Override
-    public String addUsers() {
-        IntStream.range(0,5)
-                .mapToObj(i -> UtilServiceImpl.getInstance().createRandomUsername())
-                .forEach(i -> users.put(i, User.builder()
-                        .username(i)
-                        .password("1")
-                        .name(UtilServiceImpl.getInstance().createRandomName())
-                        .job(UtilServiceImpl.getInstance().createRandomJob())
-                        .build()));
-        return users.size()+"개 더미값 추가";
-
+    public Messenger getUserMap() {
+        return null;
     }
 }
