c0755a33c98f704bf55cd171658dcec19b5bade9
tjrdbfl <tjrdbfl123@gmail.com>
2024-03-14 11:05:12 +0900

    initial commit

---
+++ test/com/dennis/api/product/Item.java
@@ -0,0 +1,11 @@
+package com.dennis.api.product;
+
+public class Item {
+    public String systemOut(){
+        return "Hello";
+    }
+    public int add(int a, int b){
+        return a+b;
+    }
+
+}
